469888dfba5de73729ac4c0a18b8cd6242d8f52f
pursam <pursam3699@gmail.com>
2022-12-12 15:25:40 +0200

    Element and Componet difference

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
-  return <div className="App"></div>;
+// React Element
+// const title = <h1>Hello React.js</h1>
+
+const List = () => {
+    return (
+        <ul>
+            <li>list item 1</li>
+            <li>list item 2</li>
+            <li>list item 3</li>
+        </ul>
+    )
 }
 
-export default App;
+// React Component
+const Title = () => <h1>Hello React component</h1>
+
+function App() {
+    return (
+        <div className="App">
+            <Title />
+            <List /> {10 + 5}
+        </div>
+    )
+}
+
+export default App
