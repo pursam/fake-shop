1636ff475953991ae0988dedd764b33000dbd18b
pursam <pursam3699@gmail.com>
2022-12-13 20:49:12 +0200

    add StyledEngineProvider

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -3,17 +3,18 @@ import React from 'react'
 import Header from '../Header/Header'
 import Main from '../Main/Main'
 import Footer from '../Footer/Footer'
+import { StyledEngineProvider } from '@mui/material'
 
 type Props = {}
 
 const App = (props: Props) => {
     return (
-        <>
+        <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header />
             <Main />
             <Footer />
-        </>
+        </StyledEngineProvider>
     )
 }
 
--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -7,13 +7,14 @@ import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import Container from '@mui/material/Container'
 import CartHeader from 'components/Cart/CartHeader'
+import './Header.scss'
 
 type Props = {}
 
 const Header = (props: Props) => {
     return (
         <>
-            <AppBar position="static">
+            <AppBar position="static" className="app-bar">
                 <Container maxWidth="lg">
                     <Toolbar>
                         <IconButton
