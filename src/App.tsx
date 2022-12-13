33fad359c0fd2c4a8ec96bda146c877fc85c89fa
pursam <pursam3699@gmail.com>
2022-12-13 13:50:05 +0200

    forms writing export

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,9 @@
 import React from 'react'
 
-const List = () => {
-    return (
-        <>
-            <ul>
-                <li>list item 1</li>
-                <li>list item 2</li>
-                <li>list item 3</li>
-            </ul>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Deserunt laborum eos optio at voluptatum. Sint quasi alias quos
-                velit, suscipit necessitatibus vero. Facere, explicabo!
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Deserunt laborum eos optio at voluptatum. Sint quasi alias quos
-                velit, suscipit necessitatibus vero. Facere, explicabo!
-            </p>
-        </>
-    )
-}
+type Props = {}
 
-type TitleProps = {
-    title: string
-    text?: string
-    num?: number
-}
-
-// React Component
-const Title = (props: TitleProps) => {
-    console.log(props)
-    return (
-        <>
-            <h1>
-                {props.text} {props.title}
-            </h1>
-            <p>{props.num}</p>
-        </>
-    )
-}
-
-function App() {
-    return (
-        <div className="App">
-            <Title text="Hello" title="App.js" num={10} />
-            <Title text="Hello" title="Test.js" num={15} />
-            <List />
-        </div>
-    )
+const App = (props: Props) => {
+    return <div>App</div>
 }
 
 export default App
